f05e7cf8f42419949aaf36b663f7a9ce5fe37992
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-02-04 20:01:43 -0600

    set up tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<>
+			<h1
+				style={{ fontFamily: "robert-medium, sans-serif" }}
+				className="text-5xl text-violet-300 font-bold"
+			>
+				Hello world!
+			</h1>
+		</>
+	);
+}
+
+export default App;
