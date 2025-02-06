aa38e4bf92e9572914ebee37e9cb4ec26dde3706
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-02-05 21:54:37 -0600

    added copy and text on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
 	const [currentIndex, setCurrentIndex] = useState(1);
@@ -36,23 +38,27 @@ const Hero = () => {
 				id="video-frame"
 				className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
 			>
-				<div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-					<div
-						onClick={handleMiniVdClick}
-						className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-					>
-						<video
-							ref={nextVideoRef}
-							src={getVideoSrc((currentIndex % totalVideos) + 1)}
-							loop
-							muted
-							id="current-video"
-							className="size-64 origin-center scale-150 object-cover object-center"
-							onLoadedData={handleVideoLoad}
-						/>
+				<div>
+					<div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+						<div
+							onClick={handleMiniVdClick}
+							className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+						>
+							<video
+								// mini video
+								ref={nextVideoRef}
+								src={getVideoSrc(
+									(currentIndex % totalVideos) + 1
+								)}
+								loop
+								muted
+								id="current-video"
+								className="size-64 origin-center scale-150 object-cover object-center"
+								onLoadedData={handleVideoLoad}
+							/>
+						</div>
 					</div>
-				</div>
-				<video
+					{/* <video
 					ref={nextVideoRef}
 					src={getVideoSrc(currentIndex)}
 					loop
@@ -60,17 +66,52 @@ const Hero = () => {
 					id="next-video"
 					className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
 					onLoadedData={handleVideoLoad}
-				/>
+				/> */}
+					<video
+						// background video
+						src={getVideoSrc(currentIndex)}
+						autoPlay
+						loop
+						muted
+						className="absolute left-0 top-0 size-full object-cover object-center"
+						onLoadedData={handleVideoLoad}
+					/>
+				</div>
 
-				<video
-					src={getVideoSrc(currentIndex)}
-					autoPlay
-					loop
-					muted
-					className="absolute left-0 top-0 size-full object-cover object-center"
-					onLoadedData={handleVideoLoad}
-				/>
+				<h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+					G<b>a</b>ming
+				</h1>
+
+				<div className="absolute left-0 top-0 z-40 size-full">
+					<div className="mt-24 px-5 sm:px-10">
+						<h1
+							// style={{ fontFamily: "special-font, sans-serif" }}
+							className="special-font hero-heading text-blue-100"
+						>
+							redefi<b>n</b>e
+						</h1>
+						<p
+							// style={{ fontFamily: "robert-regular, sans-serif" }}
+							className="mb-5 max-w-64 text-blue-100 font-robert-regular"
+						>
+							Enter the Metagame Layer <br /> Unleash the Play
+							Economy
+						</p>
+
+						<Button
+							id="watch-trailer"
+							title="Watch Trailer"
+							leftIcon={<TiLocationArrow />}
+							// rightIcon={<></>}
+							containerClass="!bg-yellow-300 flex-center gap-1"
+						/>
+					</div>
+				</div>
 			</div>
+
+			<h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+				G<b>A</b>MING
+			</h1>
 		</div>
 	);
 };
